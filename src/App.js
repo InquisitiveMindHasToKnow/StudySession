a4f34761e9045f733281222033a1ea76f8611919
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-07-31 23:32:20 -0400

    Changes button colors on answer selection. Green if right, red for all incorrect ones.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/quiz/:category" element={<Quiz />} />
-            </Routes>
-        </Router>
-    );
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/quiz/:category" element={<Quiz />} />
+          </Routes>
+      </Router>
+  );
 };
-
 export default App;
